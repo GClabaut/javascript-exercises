6e7a76bd658537d5172bb363fd7697f6f14874dd
GClabaut <123895070+GClabaut@users.noreply.github.com>
2024-04-26 10:10:58 +0200

    Finished fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+// // is using the rest operator. If this is unfamiliar to you look it up!
+// const removeFromArray = function (array, ...args) {
+//     // create a new empty array
+//     const newArray = [];
+//     // use forEach to go through the array
+//     array.forEach((item) => {
+//       // push every element into the new array
+//       // UNLESS it is included in the function arguments
+//       // so we create a new array with every item, except those that should be removed
+//     if (!args.includes(item)) {
+//         newArray.push(item);
+//     }
+//     });
+//     // and return that array
+//     return newArray;
+// };
 
-};
+var removeFromArray = function(array, ...args) {
+    return array.filter(val => !args.includes(val))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
