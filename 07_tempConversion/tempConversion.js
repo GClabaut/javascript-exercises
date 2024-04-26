84239f3635bfb82f31ae682263754b1d7032cf0f
GClabaut <123895070+GClabaut@users.noreply.github.com>
2024-04-26 11:01:30 +0200

    Finished seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let celcius = ((fahr - 32) * (5 / 9)).toFixed(1);
+  celcius = parseFloat(celcius);
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celc) {
+  let fahrenheit = ((celc * (9 / 5) + 32)).toFixed(1);
+  fahrenheit = parseFloat(fahrenheit);
+  return fahrenheit;
 };
 
 // Do not edit below this line
