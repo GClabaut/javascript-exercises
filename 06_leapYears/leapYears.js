ce8b2ee29ae7375601ad35a582c67461e88fb9b0
GClabaut <123895070+GClabaut@users.noreply.github.com>
2024-04-26 10:42:30 +0200

    Finished sixth exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // four = year % 4;
+    // hundred = year % 100;
+    // fourHundred = year % 400;
+    // if (hundred === 0 && fourHundred === 0) {
+    //     return true;
+    // } else if (hundred === 0 && four === 0) {
+    //     return false;
+    // } else if (four === 0) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
 
-};
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
+
+}
 
 // Do not edit below this line
 module.exports = leapYears;
